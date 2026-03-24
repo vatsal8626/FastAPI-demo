631961b2f9245ce56c4f4ce337fe2ec5d65f3f7a
vatsal8626 <vatsals.bitcoding@gmail.com>
2026-03-24 09:34:47 +0530

    third

--- Blog/schemas.py
+++ Blog/schemas.py
@@ -31,10 +31,3 @@ class ShowBlog(BaseModel):
 class Login(BaseModel):
     username: str
     password: str
-    
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    
-class TokenData(BaseModel):
-    username: Optional[str] = None
